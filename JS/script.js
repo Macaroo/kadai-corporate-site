dfcd873dff7908a0d7ce3de4a700539348e57fca
Macaroo <kmacaro67j2@gmail.com>
2023-01-10 15:54:40 +0900

    JS追加

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+//ハンバーガーメニュー
+$(function () {
+    $('#js-drawer').click(function () {
+    //toggleClassでclassをつけたり外したりする
+    $('body').toggleClass('is-drawerActive')
+    //attr()でaria-expandedがfalseの場合、thisはクリックした要素そのもの
+    if ($(this).attr('aria-expanded') == 'false') {
+    $(this).attr('aria-expanded', true)
+    //jQueryでcss操作、アニメーションにはdisplayではなく、visibilityを使う
+    $('#drawer-menu').css('visibility', 'visible')
+    $('#drawer-menu').attr('aria-hidden','false')
+} else {
+    $(this).attr('aria-expanded', false)
+    $('#drawer-menu').css('visibility', 'hidden')
+    $('#drawer-menu').attr('aria-hidden','true')
+    }
+})
+});
