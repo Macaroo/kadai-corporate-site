a5f044eba12d3a814471f4846aece3f0d1934985
Macaroo <kmacaro67j2@gmail.com>
2023-02-07 22:24:54 +0900

    JS修正

--- JS/script.js
+++ JS/script.js
@@ -11,8 +11,8 @@ const swiper = new Swiper('.js-brand-list', {
     loop: true, //ループさせる
     effect: "fade", //フェードのエフェクト
     autoplay: {
-    delay: 4000, //４秒後に次の画像へ
-    disableOnInteraction: false //ユーザー操作後に自動再生を再開する
+    delay: 2000, //2秒後に次の画像へ
+    disableOnInteraction: false, //ユーザー操作後に自動再生を再開する
     },
 });
 });
